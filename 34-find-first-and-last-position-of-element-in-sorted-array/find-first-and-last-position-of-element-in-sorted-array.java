b3b39c70f12696af14c6f6b2e497e1e1b38d8a16
NISHANT SINGH <156109841+nishaantttt@users.noreply.github.com>
2025-07-28 23:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (29.36%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,29 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
-        result[0] = findFirst(nums, target);
-        result[1] = findLast(nums, target);
-        return result;
+        int left = findBound(nums, target, true);
+        int right = findBound(nums, target, false);
+        return new int[] {left, right};
     }
-    
-    private int findFirst(int[] nums, int target) {
-        int index = -1;
+
+    // Helper function for binary search
+    private int findBound(int[] nums, int target, boolean isLeft) {
         int left = 0, right = nums.length - 1;
+        int bound = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) right = mid - 1;
-            else left = mid + 1;
-            if (nums[mid] == target) index = mid;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                bound = mid;
+                if (isLeft) {
+                    right = mid - 1;  // Search left part
+                } else {
+                    left = mid + 1;   // Search right part
+                }
+            }
         }
-        return index;
-    }
-    
-    private int findLast(int[] nums, int target) {
-        int index = -1;
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) left = mid + 1;
-            else right = mid - 1;
-            if (nums[mid] == target) index = mid;
-        }
-        return index;
+        return bound;
     }
 }
