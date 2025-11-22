74faf04abfa17b30269de84ecbd1afaa561842ac
NISHANT SINGH <156109841+nishaantttt@users.noreply.github.com>
2025-11-22 09:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (32.26%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,33 +1,29 @@
 class Solution {
 
-    public int solve(int i, int[] nums, int[] dp) {
-        if (i == 0) return nums[0];
-        if (i < 0) return 0;
-        if (dp[i] != -1) return dp[i];
+    private int solve(int[] arr){
+        int n = arr.length;
+        if(n == 1) return arr[0];
 
-        int pick = nums[i] + solve(i - 2, nums, dp);
-        int skip = solve(i - 1, nums, dp);
-
-        return dp[i] = Math.max(pick, skip);
-    }
-
-    public int robLinear(int[] nums) {
-        int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return solve(n - 1, nums, dp);
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+
+        for(int i = 2; i < n; i++){
+            int pick = arr[i] + dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = Math.max(pick, notPick);
+        }
+
+        return dp[n-1];
     }
 
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
+        if(n == 1) return nums[0];
 
-        // Case 1: ignore last house
-        int[] case1 = Arrays.copyOfRange(nums, 0, n - 1);
-
-        // Case 2: ignore first house
+        int[] case1 = Arrays.copyOfRange(nums, 0, n-1);
         int[] case2 = Arrays.copyOfRange(nums, 1, n);
 
-        return Math.max(robLinear(case1), robLinear(case2));
+        return Math.max(solve(case1), solve(case2));
     }
 }
