37907aa98436a013dbd7c5605a8cb345a954ccd4
NISHANT SINGH <156109841+nishaantttt@users.noreply.github.com>
2026-02-09 22:17:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (6.38%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,23 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<>()); // Start with empty subset
-
-        for (int i = 0; i < nums.length; i++) {
-            int currentSize = result.size();
-            for (int j = 0; j < currentSize; j++) {
-                List<Integer> subset = new ArrayList<>(result.get(j)); // Copy existing subset
-                subset.add(nums[i]); // Add current number
-                result.add(subset); // Add new subset to result
-            }
+    public void solve(int index, int[] nums, List<Integer> curr, List<List<Integer>> ans) {
+        if (index == nums.length) {
+            ans.add(new ArrayList<>(curr));
+            return;
         }
 
-        return result;
+        // Include
+        List<Integer> include = new ArrayList<>(curr);
+        include.add(nums[index]);
+        solve(index + 1, nums, include, ans);
+
+        // Exclude
+        List<Integer> exclude = new ArrayList<>(curr);
+        solve(index + 1, nums, exclude, ans);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(0, nums, new ArrayList<>(), ans);
+        return ans;
     }
 }
